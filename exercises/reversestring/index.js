9371109511ff33f08cb67e9cd25dde71e52a8651
s-r-aman <s.r.aman.sra@gmail.com>
2018-03-23 13:18:40 +0530

    Did String Reversals.

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,33 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  let { length } = str;
+  let newStr;
+  for (length; length >= 0; length--) {
+    if (newStr) {
+      newStr = newStr + str[length];
+    } else {
+      newStr = str[length];
+    }
+  }
+  return newStr;
+}
 
-module.exports = reverse;
+function reverse2(str) {
+  return str
+    .split('')
+    .reverse()
+    .join('');
+}
+
+function reverse3(str) {
+  let newStr = '';
+  for (const character of str) {
+    newStr = character + newStr;
+  }
+  return newStr;
+}
+
+const reverse4 = str => str.split('').reduce((prev, curr) => curr + prev, '');
+module.exports = reverse4;
