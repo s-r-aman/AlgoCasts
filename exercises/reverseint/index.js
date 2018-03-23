2915a63e0409d8ddf5592518c8c0aa4dcc9e9fcc
s-r-aman <s.r.aman.sra@gmail.com>
2018-03-23 13:19:46 +0530

    Did Integer reversal

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,24 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  const numberString = n + '';
+  const numberRevString = numberString
+    .split('')
+    .reduce((prev, curr) => curr + prev);
+  if (n > 0) return parseInt(numberRevString, 10);
+  else {
+    const num = parseInt(numberRevString, 10);
+    return num - 2 * num;
+  }
+}
 
-module.exports = reverseInt;
+function reverseInt2(n) {
+  const numberRevString = n
+    .toString()
+    .split('')
+    .reduce((prev, curr) => curr + prev);
+  return parseInt(numberRevString) * Math.sign(n);
+}
+
+module.exports = reverseInt2;
