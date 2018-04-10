e9de1589d83669dad41f29b34312e272cb33eeb8
s-r-aman <s.r.aman.sra@gmail.com>
2018-04-10 23:42:22 +0530

    Did chunks example.

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,43 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk1(array, size) {
+  let temp = [];
+  for (let index = 0; index < array.length; index++) {
+    if (!temp[temp.length - 1] || temp[temp.length - 1].length === size) {
+      temp.push([array[index]]);
+    } else if (temp[temp.length - 1].length === size) {
+      temp.push([array[index]]);
+    } else {
+      temp[temp.length - 1].push(array[index]);
+    }
+  }
+  return temp;
+}
+
+function chunk2(array, size) {
+  let temp = [];
+  for (const element of array) {
+    if (!temp[temp.length - 1]) {
+      temp.push([element]);
+    } else if (temp[temp.length - 1].length === size) {
+      temp.push([element]);
+    } else {
+      temp[temp.length - 1].push(element);
+    }
+  }
+  return temp;
+}
+
+function chunk(array, size) {
+  let temp = [];
+  let index = 0;
+  for (const iterator of array) {
+    if (array.length <= index) break;
+    temp.push(array.slice(index, index + size));
+    index = index + size;
+  }
+  return temp;
+}
 
 module.exports = chunk;
