aba299b19868b2fce83fb3950e98a827072431c5
s-r-aman <s.r.aman.sra@gmail.com>
2018-04-11 22:01:33 +0530

    Did capitilization of first letter  of word in sentence.

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,27 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize1(str) {
+  return str
+    .split(' ')
+    .map(word => {
+      return word.slice(0, 1).toUpperCase() + word.slice(1);
+    })
+    .join(' ');
+}
+
+function capitalize(str) {
+  let result = str[0].toUpperCase();
+
+  for (let index = 1; index < str.length; index++) {
+    if (str[index - 1] === ' ') {
+      result = result + str[index].toUpperCase();
+    } else {
+      result = result + str[index];
+    }
+  }
+
+  return result;
+}
 
 module.exports = capitalize;
